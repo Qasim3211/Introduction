ef51bd3e29295312a1a3c58929e572dd3870f258
Qasim3211 <160358781+Qasim3211@users.noreply.github.com>
2025-03-23 04:31:11 -0700

    Add files via upload
    Intro

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("INTRODUCTION")
+st.header("Hi I'm Qasim")
+st.subheader("I'm currently learning Artificial Intelligence, Web 3.0 & Metaverse")
+st.subheader( "You may reach me @ https://github.com/Qasim3211")
